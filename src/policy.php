0276241e495ff347d949c11722f3e4c2230f99b2
Seniru <senirupasan@gmail.com>
2024-04-28 20:01:23 +0530

    Add policy
    Co-authored-by: dewmirajapakshe

---
+++ src/policy.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!--google fonts-->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+    <!--component styles-->
+    <link rel="stylesheet" href="../styles/components.css">
+    <!--font awesomem-->
+    <script src="https://kit.fontawesome.com/36fdbb8e6c.js" crossorigin="anonymous"></script>
+    <title>Document</title>
+</head>
+
+<body>
+    <?php require("./views/header.php") ?>
+
+    <!-- body content -->
+    <h3>Terms & Condition</h3>
+    <div>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tenetur perspiciatis nesciunt laudantium odio, reprehenderit quibusdam neque sequi iste, possimus culpa, at molestias alias optio. Omnis possimus quisquam vero ut et.
+    </div>
+    <ul>
+        <li>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tenetur perspiciatis nesciunt laudantium odio, reprehenderit quibusdam neque sequi iste, possimus culpa, at molestias alias optio. Omnis possimus quisquam vero ut et.</li>
+        <li>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tenetur perspiciatis nesciunt laudantium odio, reprehenderit quibusdam neque sequi iste, possimus culpa, at molestias alias optio. Omnis possimus quisquam vero ut et.</li>
+        <li></li>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tenetur perspiciatis nesciunt laudantium odio, reprehenderit quibusdam neque sequi iste, possimus culpa, at molestias alias optio. Omnis possimus quisquam vero ut et.</li>
+    </ul>
+
+    <h3>Scope of our service:</h3>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tenetur perspiciatis nesciunt laudantium odio, reprehenderit quibusdam neque sequi iste, possimus culpa, at molestias alias optio. Omnis possimus quisquam vero ut et.</p>
+
+    <h3>Our responsibilities:</h3>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tenetur perspiciatis nesciunt laudantium odio, reprehenderit quibusdam neque sequi iste, possimus culpa, at molestias alias optio. Omnis possimus quisquam vero ut et.</p>
+
+    <h3>Cancellation policy:</h3>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tenetur perspiciatis nesciunt laudantium odio, reprehenderit quibusdam neque sequi iste, possimus culpa, at molestias alias optio. Omnis possimus quisquam vero ut et.</p>
+    <?php require("./views/footer.php") ?>
+
+</body>
+</html>
