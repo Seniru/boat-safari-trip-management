87478595729112bcd49a8ceb70f274ef7b3a14fe
Seniru <senirupasan@gmail.com>
2024-05-05 23:00:34 +0530

    Implement admin dashboard

--- src/admin-dashboard.php
+++ src/admin-dashboard.php
@@ -26,12 +26,6 @@
         <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
         <script src="../scripts/expenses-chart.js"></script>
         <title>Admin Dashboard</title>
-        <script>
-            document.addEventListener("DOMContentLoaded", function() {
-                const ctx = document.getElementById("chart")
-                createChart(ctx, [100, 300, 200, 10], ["Expense #1", "Expense #2", "Expense #3", "Expense #4"])
-            })
-        </script>
     </head>
     <body>
         <?php require("./views/header.php") ?>
@@ -42,7 +36,7 @@
                 <div class="container">
                     <h3>Add New Images</h3>
                     <br>
-                    <img src="/"></img>
+                    <i class="fa-solid fa-images" style="font-size: 100px; text-align: center;"></i>
                 </div>
                 <div class="container">
                     <h3>View feedback</h3>
@@ -65,25 +59,38 @@
                     <div class="container">
                         <h3>Add New Packages</h3>
                         <br>
-                        <img src="/"></img>
+                        <i class="fa-solid fa-leaf" style="text-align: center; font-size: 100px;"></i>
                         <br>
-                        <p>
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                        </p>    
+                        <button class="view-button"><i class="fa-solid fa-circle-plus"></i> Add</button>    
                         <i class="font-awesome">+</i>
                     </div>
-                    <div class="container">
-                        <h3>View trips</h3>
-                        <div class="trip">Trip #1</div>
-                        <div class="trip">Trip #2</div>
-                        <div class="trip">Trip #3</div>
+                    <?php
+                        $trip = $conn->query("SELECT * FROM Trip t, Location l, BoatType b, User u
+                            WHERE t.LocationID = l.LocationID AND t.BoatTypeID = b.BoatTypeID AND t.UserID = u.UserID
+                            ORDER BY t.TripID DESC
+                            LIMIT 1
+                    ")->fetch_assoc();
+                        ?>
+                    <div id="latest-trip" class="container">
+                        <h3>Latest trip</h3>
+                        <br>
+                        <?php echo $trip["FirstName"] ?>
+                        <br><br>
+                        <i class="fa-solid fa-location-dot fa-fw"></i>
+                        <?php echo $trip["LocationName"] ?><br>
+                        <i class='fa-solid fa-sailboat'></i>
+                        <?php echo $trip["BoatTypeName"] ?><br><br>
                         <button class="view-button">View</button>
                     </div>
-                    <div class="container">
+                    <div style="display: inline-block; vertical-align: top;" class="container">
                         <h3>Payment details</h3>
-                        Username<br>
-                        Bank Details<br>
-                        Date/Time<br>
+                        <?php echo $trip["FirstName"] ?><br><br>
+                        <i class="fa-solid fa-building-columns"></i>
+                        <?php echo $trip["PaymentMode"] ?><br>
+                        <i class="fa-solid fa-money-bill"></i>
+                        $<?php echo $trip["PaymentAmoount"] ?><br><br>
+                        <i class="fa-solid fa-calendar"></i>
+                        <?php echo $trip["DateTime"] ?><br>
                         <button class="view-button">View</button>
                     </div>
                     <div class="container">
@@ -95,29 +102,44 @@
                 </section>
             </div>
             <section id="section4">
+                <?php
+                    $trip_income = $conn->query("SELECT SUM(PaymentAmoount) AS TripIncome FROM Trip")->fetch_assoc()["TripIncome"];
+                    $trip_provider_salaries = $conn->query("SELECT COUNT(*) AS Providers FROM TripProvider")->fetch_assoc()["Providers"] * 100;
+                    $support_agent_salaries = $conn->query("SELECT COUNT(*) AS Agents FROM CustomerSupportAgent")->fetch_assoc()["Agents"] * 60
+                ?>
                 <h2>Finances</h2>
                 <section id="revenue-details">
                     <div class="container">
                         <h3>Revenue</h3>
                         <b>Trip income</b>
-                        <span class="money">$99.99</span><br>
+                        <span class="money">$<?php echo $trip_income; ?></span><br>
                         <b>Expenses</b>
-                        <span class="money">$99.99</span><br>
+                        <span class="money">$<?php echo ($trip_provider_salaries + $support_agent_salaries + 25) ?></span><br>
                         <div id="expenses">
-                            Expense #1
-                            <span class="money">$99.99</span><br>
-                            Expense #2
-                            <span class="money">$99.99</span><br>
-                            Expense #3
-                            <span class="money">$99.99</span><br>
+                            Trip provider salaries
+                            <span class="money">$<?php echo $trip_provider_salaries; ?></span><br>
+                            Support agent salaries
+                            <span class="money">$<?php echo $support_agent_salaries; ?></span><br>
+                            System maintainence
+                            <span class="money">$25.00</span><br>
                         </div>
                         <br>
                         <b>Net income</b>
-                        <span class="money">$99.99</span><br>
+                        <span class="money">$<?php echo $trip_income - ($trip_provider_salaries + $support_agent_salaries + 25); ?></span><br>
                     </div>
                 </section>
                 <section id="expenses-chart">
-                    <canvas id="chart"> </canvas>
+                    <script>
+                        document.addEventListener("DOMContentLoaded", function() {
+                        const ctx = document.getElementById("chart")
+                        createChart(ctx, [
+                            <?php echo $trip_provider_salaries ?>, 
+                            <?php echo $support_agent_salaries ?>,
+                            25
+                        ], ["Trip provider salaries", "Support agent salaries", "System maintainence"])
+                        })
+                    </script>
+                    <canvas id="chart"></canvas>
                 </section>
             </section>
         <?php require("./views/footer.php") ?>
