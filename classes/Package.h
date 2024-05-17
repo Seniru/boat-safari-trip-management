abed060f5bf22d87a558a6b17c28129f7907ce2c
ShashiprabhaIlankoon <164845135+ShashiprabhaIlankoon@users.noreply.github.com>
2024-05-17 08:15:56 +0530

    Update Package.h

--- classes/Package.h
+++ classes/Package.h
@@ -1,5 +1,15 @@
 #pragma once
 
 class Package {
-
+     private:
+         int PackageID;
+         string PackageName[20];
+         float PackagePrice;
+         string Facilities;
+         string BoatType;
+     public:
+         Package(int pid, string pName, float pPrice, string pFacilities, string pBoatType);
+         void setPackageDetails();
+         void displayPackageDetails();
+         ~Package();
 };
